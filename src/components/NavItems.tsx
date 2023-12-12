67ae57cdc8bafa81bba7da52ca3d57d747b59a4b
Jesus Torres <torres.jesus2143@gmail.com>
2023-12-11 23:59:28 -0600

    added navbar plus hooks folder

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,13 +1,33 @@
 "use client";
 
-import { useState } from "react";
+import { PRODUCT_CATEGORIES } from "@/config";
+import { useState, useRef } from "react";
+import NavItem from "./NavItem";
 
 const NavItems = () => {
 	const [activeIndex, setActiveIndex] = useState<null | number>(null);
 
+	const isAnyOpen = activeIndex !== null;
+
+	const navRef = useRef<HTMLDivElement | null>(null);
+
 	return (
 		<div className="flex gap-4 h-full">
-			<p>Hello World</p>
+			{PRODUCT_CATEGORIES.map((category, i) => {
+				const handleOpen = () => {
+					if (activeIndex === i) {
+						setActiveIndex(null);
+					} else {
+						setActiveIndex(i);
+					}
+				};
+
+				const isOpen = i === activeIndex;
+
+				return (
+					<NavItem category={category} handleOpen={handleOpen} isOpen={isOpen} key={category.value} isAnyOpen={isAnyOpen} />
+				);
+			})}
 		</div>
 	);
 };
